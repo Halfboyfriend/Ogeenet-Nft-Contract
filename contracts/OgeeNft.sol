9759c9b17fc19c7273f6dd1a9664f91e60f74386
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-09-19 18:42:13 +0100

    nft contract

--- contracts/OgeeNft.sol
+++ contracts/OgeeNft.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.18;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract OgeeNetNft{
-    
+contract OgeeNetNF is ERC721URIStorage{
+    address public owner;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "only owner can call");
+        _;
+    }
+    constructor() ERC721("OgeeNet NFT", "OG"){
+        owner = msg.sender;
+    }
+    function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner{
+        _mint(_to, _tokenId);
+        _setTokenURI(_tokenId, _uri);
+    }
+
 }
